82d29f130423894c0fd053f0236a136d6c48eced
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2022-11-21 12:34:11 -0300

    Conversor de temperatura
    Enviando todos os arquivos

---
+++ ConversrTemperaturas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversrTemperaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("###CONVERSOR DE TEMPERATURA###");
+            Console.Write("Insira a temperatura em Celcius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("---------------");
+
+            // (c * 9 / 5) + 32 = F
+            f = (c * 9 / 5) + 32;
+
+            // c + 273,15
+            k = c + 273.15;
+
+            Console.WriteLine(c + " graus celcius = " + f + " graus Fahrenheit" + " e " + k + " graus Kelvin");
+            Console.ReadKey();
+
+        }
+    }
+}
